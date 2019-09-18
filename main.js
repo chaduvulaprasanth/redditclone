a3f222e9253470bc7b871a5c06e58363d1a1df2e
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-09-18 21:57:26 +0530

    updated reddit clone

---
+++ main.js
@@ -0,0 +1,21 @@
+var ul = document.querySelector(".ul");
+var wholeinfo = [];
+
+function display(arr){
+    ul.innerHTML = arr.map(( item, index) => {
+        return (
+            `<li class = "list">
+                <p class="postedby">Posted by u/${item.data.author}</p>
+                <p class="post-title">${item.data.title}</p>
+                <p class="self-text">${item.data.selftext}</p>
+            </li>`
+        )
+    });
+    // }).join(" ");
+}
+function getapi(){
+    fetch(`http://www.reddit.com/search.json?q=startup`)
+        .then(res => res.json())
+        .then(data => display(data.data.children));
+}
+getapi();
